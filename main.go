3cb0f1ebaa5701f8b19d47b197ed125d3b0db5a4
Adam Dyszy <electro.ubro@gmail.com>
2018-12-03 00:51:02 +0100

    voting works live with updates from client to server

--- main.go
+++ main.go
@@ -2,72 +2,102 @@ package main
 
 import (
 	"github.com/kataras/iris"
-	"github.com/kataras/iris/middleware/logger"
-	"github.com/kataras/iris/middleware/recover"
 	"math/rand"
 	"strconv"
 	"sync"
 	"time"
 )
 
-//func say(s chan string) {
-//	for ; ;  {
-//		time.Sleep(time.Second*5)
-//		s <- "forward"
-//		time.Sleep(time.Second*5)
-//		s <- "backward"
-//	}
-//}
-func generateVotes(s *SafeCommand) {
+func generateVotes(s *SafeCommands) {
 	for {
 		s.mux.Lock()
-		valTmp, _ := strconv.Atoi(s.votes[0])
-		s.votes[0] = strconv.Itoa(valTmp + rand.Intn(3))
-		valTmp, _ = strconv.Atoi(s.votes[1])
-		s.votes[1] = strconv.Itoa(valTmp + rand.Intn(3))
-		valTmp, _ = strconv.Atoi(s.votes[2])
-		s.votes[2] = strconv.Itoa(valTmp + rand.Intn(3))
-		valTmp, _ = strconv.Atoi(s.votes[3])
-		s.votes[3] = strconv.Itoa(valTmp + rand.Intn(3))
+		for command := 0; command < commandSize; command++ {
+			valTmp, _ := strconv.Atoi(s.votes[command])
+			s.votes[command] = strconv.Itoa(valTmp + rand.Intn(3))
+		}
 		s.mux.Unlock()
 		time.Sleep(time.Second * 1)
-
 	}
 }
 
-func resetVotes(s *SafeCommand) {
+func resetVotes(s *SafeCommands) {
 	for {
 		s.mux.Lock()
-		s.votes = [4]string{"0", "0", "0", "0"}
+		curMax := 0
+		newCommand := idle // if no votes then idle
+		votes := s.votes
+		perm := rand.Perm(commandSize) // we always take random permutation so there are no favourites
+		for _, command := range perm {
+			valTmp, _ := strconv.Atoi(votes[command])
+			if valTmp > curMax {
+				curMax = valTmp
+				newCommand = Command(command)
+			}
+		}
+		s.votes = [5]string{"0", "0", "0", "0", "0"}
+		s.lastCommand = newCommand
 		s.mux.Unlock()
 		time.Sleep(time.Second * 10)
 	}
 }
 
+type Command int
+
+const commandSize = 5
+const (
+	idle     Command = 0
+	forward  Command = 1
+	backward Command = 2
+	left     Command = 3
+	right    Command = 4
+)
+
+func String(command Command) string {
+	names := [...]string{
+		"idle",
+		"forward",
+		"backward",
+		"left",
+		"right"}
+	return names[command]
+}
+func stringToCommand(s string) Command {
+	switch s {
+	case "idle":
+		return idle
+	case "forward":
+		return forward
+	case "backward":
+		return backward
+	case "left":
+		return left
+	case "right":
+		return right
+	default:
+		return -1
+	}
+}
+
 // safe to use concurrently.
-type SafeCommand struct {
-	votes [4]string
-	mux   sync.Mutex
+type SafeCommands struct {
+	votes       [5]string // string because it is most often used as get
+	lastCommand Command
+	mux         sync.Mutex
 }
 
 var page = struct {
 	Title string
-}{"Welcome"}
+}{"Vote Fast!!!"}
 
 func main() {
 	app := iris.New()
-	app.Logger().SetLevel("debug")
+	//app.Logger().SetLevel("debug")
 	app.RegisterView(iris.HTML("public", ".html"))
-	// Optionally, add two built'n handlers
-	// that can recover from any http-relative panics
-	// and log the requests to the terminal.
-	app.Use(recover.New())
-	app.Use(logger.New())
 
-	// Method:   GET
-	// Resource: http://localhost:8080
-	//commandToDo := make(chan string)
-	commandToDo := SafeCommand{votes: [4]string{"0", "0", "0", "0"}}
+	//app.Use(recover.New())
+	//app.Use(logger.New())
+
+	commandsToDo := SafeCommands{votes: [5]string{"0", "0", "0", "0", "0"}, lastCommand: idle}
 
 	app.StaticWeb("/", "public")
 	app.Get("/", func(ctx iris.Context) {
@@ -75,16 +105,36 @@ func main() {
 		ctx.View("index.html")
 	})
 
+	app.Get("/getCommand", func(ctx iris.Context) {
+		//println(commandsToDo.lastCommand)
+		ctx.HTML(String(commandsToDo.lastCommand))
+	})
+
+	app.Post("/postDirection", func(ctx iris.Context) {
+		//s := `form:"direction"`
+		var s string
+		ctx.ReadForm(&s)
+		commandsToDo.mux.Lock()
+		command := stringToCommand(s)
+		if command != -1 {
+			valTmp, _ := strconv.Atoi(commandsToDo.votes[command])
+			commandsToDo.votes[command] = strconv.Itoa(valTmp + 1)
+		} else {
+			println("invalid command: " + s)
+		}
+		commandsToDo.mux.Unlock()
+	})
+
 	app.Handle("GET", "/directions", func(ctx iris.Context) {
-		commandToDo.mux.Lock()
-		ctx.HTML(commandToDo.votes[0] + "|" + commandToDo.votes[1] + "|" + commandToDo.votes[2] + "|" + commandToDo.votes[3])
-		commandToDo.mux.Unlock()
+		commandsToDo.mux.Lock()
+		ctx.HTML(commandsToDo.votes[0] + "|" + commandsToDo.votes[1] + "|" + commandsToDo.votes[2] + "|" + commandsToDo.votes[3] + "|" + commandsToDo.votes[4])
+		commandsToDo.mux.Unlock()
 	})
 	assetHandler := app.StaticHandler("public", false, false)
 
 	app.SPA(assetHandler)
 
-	go generateVotes(&commandToDo)
-	go resetVotes(&commandToDo)
+	//go generateVotes(&commandsToDo)
+	go resetVotes(&commandsToDo)
 	app.Run(iris.Addr("0.0.0.0:8080"), iris.WithoutServerError(iris.ErrServerClosed))
 }
--- public/app.js
+++ public/app.js
@@ -1,19 +1,25 @@
 var req;
+var req2;
+var reqPost;
+const urlVotes = '/directions';
+const urlCommand = '/getCommand'
+const urlPost = '/postDirection';
+const reloadCooldownMS = 1000; //cooldown every X milliseconds
+const voteWidth = 15;
 
 function reloadData()
 {
-    var now = new Date();
-    // url = 'liveData?' + now.getTime();
-    url = '/directions';
-
     try {
         req = new XMLHttpRequest();
+        req2 = new XMLHttpRequest();
     } catch (e) {
         try {
             req = new ActiveXObject("Msxml2.XMLHTTP");
+            req2 = new ActiveXObject("Msxml2.XMLHTTP");
         } catch (e) {
             try {
                 req = new ActiveXObject("Microsoft.XMLHTTP");
+                req2 = new ActiveXObject("Microsoft.XMLHTTP");
             } catch (oc) {
                 alert("No AJAX Support");
                 return;
@@ -21,72 +27,88 @@ function reloadData()
         }
     }
 
-    req.onreadystatechange = processReqChange;
-    req.open("GET", url, true);
+    req.onreadystatechange = updateVotes;
+    req.open("GET", urlVotes, true);
     req.send(null);
+    req2.onreadystatechange = updateCommand;
+    req2.open("GET", urlCommand, true);
+    req2.send(null);
 }
 
-function processReqChange()
-{
-    // If req shows "complete"
-    if (req.readyState == 4)
+function updateCommand(){
+    if (req2.readyState === XMLHttpRequest.DONE && req2.status===200)
     {
-        dataDiv = document.getElementById('currentData');
-        dataDiv2 = document.getElementById('container');
-
-        // If "OK"
-        if (req.status == 200)
-        {
-            // Set current data text
-            var resText = req.responseText;
-            dataDiv.innerHTML = resText;
-            resText=resText.split("|");
-            // dataDiv2.innerHTML = "<span id=\"question\">What direction?</span>" +
-            //     "<div><span>"+resText[0]+"</span><a href=\"\">Vote</a>Forward</div>\n" +
-            //     "    <div><span>"+resText[1]+"</span><a href=\"\">Vote</a>Backward</div>\n" +
-            //     "    <div><span>"+resText[2]+ "</span><a href=\"\">Vote</a>Turn Left</div>\n" +
-            //     "    <div><span>"+resText[3]+"</span><a href=\"\">Vote</a>Turn Right</div>"+
-            // "</div>";
-            forVal = document.getElementById("forward-val");
-            forVal.innerText=resText[0];
-            $(forVal).parent().animate({width:(parseInt(resText[0])*30+300)+'px'});
-            forVal = document.getElementById("backward-val");
-            forVal.innerText=resText[1];
-            $(forVal).parent().animate({width:(parseInt(resText[1])*30+300)+'px'});
-            forVal = document.getElementById("left-val");
-            forVal.innerText=resText[2];
-            $(forVal).parent().animate({width:(parseInt(resText[2])*30+300)+'px'});
-            forVal = document.getElementById("right-val");
-            forVal.innerText=resText[3];
-            $(forVal).parent().animate({width:(parseInt(resText[3])*30+300)+'px'});
-
-            // forVal.parent().animate({width:parseInt(resText[0])*100+'px'})
-            // $("#container div a").click(function() {
-            //     $(this).parent().animate({
-            //         width: '+=100px'
-            //     }, 500);
-            //
-            //     $(this).prev().html(parseInt($(this).prev().html()) + 1);
-            //     return false;
-            // });
-            // Start new timer (3 sec)
-            timeoutID = setTimeout('reloadData()', 1000);
-        }
-        else
-        {
-            // Flag error
-            dataDiv.innerHTML = '<p>There was a problem retrieving data: ' + req.statusText + '</p>';
-        }
+        document.getElementById("last-command").innerText=req2.responseText;
     }
 }
 
+function updateVotes()
+{
+    // If req shows "complete"
+    if (req.readyState === XMLHttpRequest.DONE) {
+
+        if (req.status === 200) {
+            // Set current data text
+            var resText = req.responseText;
+            resText = resText.split("|");
+            tmpElem = document.getElementById("idle-val");
+            tmpElem.innerText = resText[0];
+            $(tmpElem).parent().animate({width: (parseInt(resText[0]) * voteWidth + 300) + 'px'});
+            tmpElem = document.getElementById("forward-val");
+            tmpElem.innerText = resText[1];
+            $(tmpElem).parent().animate({width: (parseInt(resText[1]) * voteWidth + 300) + 'px'});
+            tmpElem = document.getElementById("backward-val");
+            tmpElem.innerText = resText[2];
+            $(tmpElem).parent().animate({width: (parseInt(resText[2]) * voteWidth + 300) + 'px'});
+            tmpElem = document.getElementById("left-val");
+            tmpElem.innerText = resText[3];
+            $(tmpElem).parent().animate({width: (parseInt(resText[3]) * voteWidth + 300) + 'px'});
+            tmpElem = document.getElementById("right-val");
+            tmpElem.innerText = resText[4];
+            $(tmpElem).parent().animate({width: (parseInt(resText[4]) * voteWidth + 300) + 'px'});
+            timeoutID = setTimeout('reloadData()', reloadCooldownMS);
+        }
+        else {
+            // Flag error
+            console.log('There was a problem retrieving data: ' + req.statusText);
+        }
+    }
+
+}
+
+
 $(document).ready(function() {
     $("#container div a").click(function() {
-        $(this).parent().animate({
-            width: '+=100px'
-        }, 500);
+        try {
+            reqPost = new XMLHttpRequest();
+        } catch (e) {
+            try {
+                reqPost = new ActiveXObject("Msxml2.XMLHTTP");
+            } catch (e) {
+                try {
+                    reqPost = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (oc) {
+                    alert("No AJAX Support");
+                    return;
+                }
+            }
+        }
+        reqPost.open("POST", urlPost, true);
+        reqPost.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+        reqPost.onreadystatechange = function() { // Call a function when the state changes.
+            if (this.readyState === XMLHttpRequest.DONE){
+                if (this.status === 200) {
+                    // reloadData();
+                    // don't reload since reloading is often enough
+                }
+                else{
+                    console.log('There was a problem post\'ing data: ' + this.statusText);
+                }
+            }
+        };
 
-        $(this).prev().html(parseInt($(this).prev().html()) + 1);
+        reqPost.send("direction="+$(this).prev()[0].id.split("-")[0]);
         return false;
     });
 });
+
